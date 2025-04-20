75c7b4c3867532ee14ffc8883c169da20e52a1ed
samarth <samarth0592002@gmail.com>
2025-04-20 20:25:03 +0530

    v1

---
+++ KafkaBroker/scripts/create_cluster_id.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+file_path="/tmp/clusterID/clusterID"
+
+if [ ! -f "$file_path" ]; then
+  /bin/kafka-storage random-uuid > /tmp/clusterID/clusterID
+  echo "Cluster id has been  created..."
+fi
